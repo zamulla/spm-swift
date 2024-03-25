a5499bfa60857e3edcd4f779d8e62808e9006435
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-03-25 16:22:37 +0400

    trying tests

--- Package.swift
+++ Package.swift
@@ -2,14 +2,15 @@
 import PackageDescription
 
 let package = Package(
-   name: "UmbrellaDependency",
+   name: "Shared",
    platforms: [
      .iOS(.v14),
    ],
    products: [
-      .library(name: "UmbrellaDependency", targets: ["together"])
+      .library(name: "Shared", targets: ["Shared"])
    ],
-   targets: [
-       .binaryTarget(name: "together", url: "https://github.com/zamulla/spmfinal/releases/download/1.0.0/together.xcframework.zip",checksum:"0c13e1223bf9de95d030ba89bbd274dac57ada43387b45a52e27244891ba5776")
+    targets: [
+        .binaryTarget(name: "Shared", url: "https://github.com/zamulla/spmfinal/releases/download/1.2.0/Shared.xcframework.zip",checksum:"66cf7090b88cf56ebd61d990c8c6c4faf1f8bdbfab3f5c1e76bf725bbdc80100"),
+        .testTarget(name: "sharedCheck", dependencies: ["Shared"])
    ]
 )
